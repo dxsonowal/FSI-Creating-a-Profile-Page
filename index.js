5d7083850c695fb512bf51ccf7245bca4df68b33
HomeMac <homemac@HomeMacs-MacBook-Pro.local>
2022-02-04 23:41:18 -0800

    final update

--- index.js
+++ index.js
@@ -20,16 +20,31 @@ div1.append(dogDetails)
 let h3Header=document.createElement('h3')
 h3Header.append('Description:')
 h3Header.style.textAlign = 'left'
-div1.append(h3Header)
+dogDetails.append(h3Header)
 
 let dogPara = document.createElement('p')
 dogPara.append(' ')
-dogPara.append('This gentle dog is aloof toward her owner, and never comes when called.She always acts as though any stranger she meets will harm her,and dislikes other animals.')
-div1.append(dogPara)
+dogPara.append('This gentle dog is aloof toward her owner, and never comes when called. She always acts as though any stranger she meets will harm her, and dislikes other animals.')
+dogDetails.append(dogPara)
 
 let feedingTimes=document.createElement('h3')
 feedingTimes.append('Feeding Times:')
 feedingTimes.style.textAlign = 'left'
-div1.append(feedingTimes)
+dogDetails.append(feedingTimes)
+
+let feedTimeul = document.createElement('ul')
+
+let feedTime = document.createElement('li')
+feedTime.textContent = '9:00 am'
+feedTimeul.appendChild(feedTime)
+
+let item2 = document.createElement('li')
+item2.textContent = '12:00 pm'
+feedTimeul.appendChild(item2)
+let item3 = document.createElement('li')
+item3.textContent = '5:00 pm'
+feedTimeul.appendChild(item3)
+
+dogDetails.append(feedTimeul)
 
 //let dogimage =document.createElement('')
